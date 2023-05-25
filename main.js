055e2ebedfe9d5bd938cddf1741f5d8cad0929d7
TANIA SAMARTINEANU <TANIA.SAMARTINEANU82@gmail.com>
2023-05-25 19:14:44 +0300

    update

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const assemblyRows = document.querySelectorAll('.assembly-row');
-
-        // Add click event listener to each assembly row
-        assemblyRows.forEach((row) => {
-            row.addEventListener('click', function () {
-                // Find the sibling dropdown element
-                const dropdown = row.nextElementSibling;
-
-                // Toggle the display of the dropdown
-                dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
-            });
-        });
+function toggleDropdown(index) {
+    const dropdown= document.getElementById(`dropdown-${index}`);
+    dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+  }
