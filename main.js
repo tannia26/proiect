3f3b9833608c83d5e7148cb8683d11019cf8848b
Emanuel Rusu <erusu364@gmail.com>
2023-05-25 20:45:11 +0300

    - dropdown - contact

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function toggleDropdown(index) {
-    const dropdown= document.getElementById(`dropdown-${index}`);
-    dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
-  }
+    const dropdown = document.getElementById(`dropdown-${index}`);
+    dropdown.style.display =
+        dropdown.style.display === 'none' ? 'block' : 'none';
+}
